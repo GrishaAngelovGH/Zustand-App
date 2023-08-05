713128c0cc0b3002fe437b98aaaf7993ee97fabf
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-05 14:01:11 +0200

    Persist store data via zustand middleware

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,52 +1,60 @@
 import { create } from "zustand"
+import { persist } from 'zustand/middleware'
 
-const useStore = create(set => ({
-  items: [
-    { id: 1, title: "Item 1", quantity: 0, marked: false },
-    { id: 2, title: "Item 2", quantity: 0, marked: false },
-    { id: 3, title: "Item 3", quantity: 0, marked: false },
-    { id: 4, title: "Item 4", quantity: 0, marked: false },
-    { id: 5, title: "Item 5", quantity: 0, marked: false }
-  ],
-  incrementQty: id => {
-    set(state => {
-      const newItems = [...state.items]
-      const item = newItems.find(v => v.id === id)
-      item.quantity += 1
+const useStore = create(
+  persist(
+    set => ({
+      items: [
+        { id: 1, title: "Item 1", quantity: 0, marked: false },
+        { id: 2, title: "Item 2", quantity: 0, marked: false },
+        { id: 3, title: "Item 3", quantity: 0, marked: false },
+        { id: 4, title: "Item 4", quantity: 0, marked: false },
+        { id: 5, title: "Item 5", quantity: 0, marked: false }
+      ],
+      incrementQty: id => {
+        set(state => {
+          const newItems = [...state.items]
+          const item = newItems.find(v => v.id === id)
+          item.quantity += 1
 
-      return ({
-        ...state,
-        items: newItems
-      })
-    })
-  },
-  decrementQty: id => {
-    set(state => {
-      const newItems = [...state.items]
-      const item = newItems.find(v => v.id === id)
+          return ({
+            ...state,
+            items: newItems
+          })
+        })
+      },
+      decrementQty: id => {
+        set(state => {
+          const newItems = [...state.items]
+          const item = newItems.find(v => v.id === id)
 
-      if (item.quantity > 0) {
-        item.quantity -= 1
+          if (item.quantity > 0) {
+            item.quantity -= 1
+          }
+
+          return ({
+            ...state,
+            items: newItems
+          })
+        })
+      },
+      markItem: id => {
+        set(state => {
+          const newItems = [...state.items]
+          const item = newItems.find(v => v.id === id)
+          item.marked = !item.marked
+
+          return ({
+            ...state,
+            items: newItems
+          })
+        })
       }
-
-      return ({
-        ...state,
-        items: newItems
-      })
-    })
-  },
-  markItem: id => {
-    set(state => {
-      const newItems = [...state.items]
-      const item = newItems.find(v => v.id === id)
-      item.marked = !item.marked
-
-      return ({
-        ...state,
-        items: newItems
-      })
-    })
-  }
-}))
+    }),
+    {
+      name: 'item-storage'
+    }
+  )
+)
 
 export default useStore
