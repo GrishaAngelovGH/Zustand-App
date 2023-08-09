fe0d3c8f1a91c50d4a1e2dcaf7021a91dba75f37
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-09 20:06:28 +0300

    Implement Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Header from "components/Header"
 import ItemList from "components/ItemList"
 
 function App() {
   return (
     <div className="row g-0 vh-100 bg-body-secondary">
       <div className="col-md-12">
-        <h1 className="text-center">Zustand App</h1>
+        <Header />
         <ItemList />
       </div>
     </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+const Header = () => (
+  <h1 className="text-secondary text-center shadow p-2">Zustand App</h1>
+)
+
+export default Header
