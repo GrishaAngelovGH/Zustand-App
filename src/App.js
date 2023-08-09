2c7f5e680552fbb400455ac0464cbafa9fb10255
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-09 20:00:09 +0300

    Change background color to gray nuance

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ItemList from "components/ItemList"
 
 function App() {
   return (
-    <div className="row g-0">
+    <div className="row g-0 vh-100 bg-body-secondary">
       <div className="col-md-12">
         <h1 className="text-center">Zustand App</h1>
         <ItemList />
